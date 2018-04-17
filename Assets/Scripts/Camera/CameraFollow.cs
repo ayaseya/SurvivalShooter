3af308cca33b5ca31ae0636ac53a206f20ae0921
ayaseya <ayaseya@gmail.com>
2018-04-17 17:41:50 +0900

    Initial commit
    Created a new tutorial project for Unity.
    https://unity3d.com/jp/learn/tutorials/s/survival-shooter-tutorial

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform target;		// カメラの追尾対象（プレイヤー）の位置
+	public float smoothing = 5f;	// カメラの追尾速度
+
+	Vector3 offset;					// カメラからtargetまでの距離、
+									// カメラは常にプレイヤーからoffset分の距離を保つことになる
+
+	void Start ()
+	{
+		// プレイヤーの初期位置からoffsetを初期化する
+		offset = transform.position - target.position;
+	}
+
+
+	void FixedUpdate ()
+	{
+		// プレイヤーの位置から追尾後のカメラ位置を算出する
+		Vector3 targetCamPos = target.position + offset;
+
+		// プレイヤーの位置を元にカメラを移動させる
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
